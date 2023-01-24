7d6b189b42adf8cd3967d3539a91105f12a14212
voskanyan03 <itgdavo@gmail.com>
2023-01-24 21:37:14 +0400

    i18n install

--- src/assets/i18n.js
+++ src/assets/i18n.js
@@ -1,7 +1,7 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import HttpApi from "i18next-http-backend";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
 import common_en from "../languages/en/translate.json";
 import common_ru from "../languages/ru/translate.json";
 
@@ -27,3 +27,6 @@ i18n
     },
     react: { useSuspense: false },
   });
+
+
+export default i18n;
