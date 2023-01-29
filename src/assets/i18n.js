afa45811b08875f428d5537385c2b0c081720a2d
sargishakobyann97 <sargishakobyann97@gmail.com>
2023-01-29 11:41:58 +0300

    fix i18n strings fotmal and import

--- src/assets/i18n.js
+++ src/assets/i18n.js
@@ -1,32 +1,30 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
 import common_en from "../languages/en/translate.json";
 import common_ru from "../languages/ru/translate.json";
 
-i18n
-  .use(initReactI18next)
-  .use(LanguageDetector)
-  .use(HttpApi)
-  .init({
-    supportedLngs: ["en", "ru"],
-    fallbackLng: "en",
-    resources: {
-      en: { common: common_en },
-      ru: { common: common_ru },
-    },
-    ns: ["common"],
-    defaultNS: "common",
-    detection: {
-      order: ["path", "subdomain", "localStorage", "cookie", "htmlTag"],
-      caches: ["localStorage"],
-    },
-    backend: {
-      loadPath: "../languages/{{lng}}/translate.json",
-    },
-    react: { useSuspense: false },
-  });
-
+i18n.use(initReactI18next)
+    .use(LanguageDetector)
+    .use(HttpApi)
+    .init({
+        supportedLngs: ["en", "ru"],
+        fallbackLng: "en",
+        resources: {
+            en: { common: common_en },
+            ru: { common: common_ru },
+        },
+        ns: ["common"],
+        defaultNS: "common",
+        detection: {
+            order: ["path", "subdomain", "localStorage", "cookie", "htmlTag"],
+            caches: ["localStorage"],
+        },
+        backend: {
+            loadPath: "../languages/{{lng}}/translate.json",
+        },
+        react: { useSuspense: false },
+    });
 
 export default i18n;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import i18n from "./assets/i18n";
+import "./assets/i18n";
 
 createRoot(document.getElementById("root")).render(<App />);
